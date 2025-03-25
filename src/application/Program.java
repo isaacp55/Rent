5c846bb5b93987431fc07f9752941a20ddd1de32
Isaac Pereira Da Silva <isaacsilva15663@gmail.com>
2025-03-25 16:12:12 -0300

    organizando arquivos e sintaxe de pacotes

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,16 +1,12 @@
 package application;
 
-<<<<<<< HEAD
 import java.text.ParseException;
 import java.time.LocalDateTime;
-=======
-import java.time.LocalDate;
->>>>>>> 659df38809444a8a972985bbb48a5d5a6b016df6
 import java.time.format.DateTimeFormatter;
+import java.util.Date;
 import java.util.Locale;
 import java.util.Scanner;
 
-<<<<<<< HEAD
 import model.entities.CarRental;
 import model.entities.Vehicle;
 import model.services.BrazilTaxService;
@@ -18,70 +14,37 @@ import model.services.RentalService;
 
 public class Program {
 
-	public static void main(String[] args)  throws ParseException{
+	public static void main(String[] args) throws ParseException {
+
 		Locale.setDefault(Locale.US);
-		Scanner scn = new Scanner(System.in);
+		Scanner sc = new Scanner(System.in);
 		
-		DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
+		DateTimeFormatter fmt =  DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
 		
-		System.out.println("Entre com os dados do aluguel: ");
-		System.out.println("Modelo do carro: ");
-		String carModel = scn.nextLine();
-		System.out.println("Retirada (dd/MM/yyyy hh:mm): ");
-		LocalDateTime start = LocalDateTime.parse(scn.nextLine(), fmt);
-		System.out.println("Retorno (dd/MM/yyyy hh:mm): ");
-		LocalDateTime finish = LocalDateTime.parse(scn.nextLine(), fmt);
-	    CarRental cr = new CarRental(start, finish, new Vehicle(carModel)); 
-	    
-	    System.out.println("Entre com o preço por hora:");
-	    double pricePerHour = scn.nextDouble();
-	    System.out.println("Entre com o preço por dia:");
-	    double pricePerDay = scn.nextDouble();
-	    
-	    RentalService rentalService = new RentalService(pricePerHour, pricePerDay, new BrazilTaxService());
+		System.out.println("Entre com os dados do aluguel");
+		System.out.print("Modelo do carro: ");
+		String carModel = sc.nextLine();
+		System.out.print("Retirada (dd/MM/yyyy HH:mm): ");
+		LocalDateTime start = LocalDateTime.parse(sc.nextLine(), fmt);
+		System.out.print("Retorno (dd/MM/yyyy HH:mm): ");
+		LocalDateTime finish = LocalDateTime.parse(sc.nextLine(), fmt);
+		
+		CarRental cr = new CarRental(start, finish, new Vehicle(carModel));
+
+		System.out.print("Entre com o preço por hora: ");
+		double pricePerHour = sc.nextDouble();
+		System.out.print("Entre com o preço por dia: ");
+		double pricePerDay = sc.nextDouble();
+		
+		RentalService rentalService = new RentalService(pricePerDay, pricePerHour, new BrazilTaxService());
+		
 		rentalService.processInvoice(cr);
-		
+
 		System.out.println("FATURA:");
-		System.out.println("Pagamento básico: " + cr.getInvoice().getBasicPayment());
-		System.out.println("Imposto: " + cr.getInvoice().getTax());
-		System.out.println("Pagamento total: " + cr.getInvoice().getTotalPayment());
+		System.out.println("Pagamento basico: " + String.format("%.2f", cr.getInvoice().getBasicPayment()));
+		System.out.println("Imposto: " + String.format("%.2f", cr.getInvoice().getTax()));
+		System.out.println("Pagamento total: " + String.format("%.2f", cr.getInvoice().getTotalPayment()));
 		
-		scn.close();
-=======
-import model.entities.Contract;
-import model.entities.Installment;
-import model.services.ContractService;
-import model.services.PaypalService;
-
-public class Program {
-
-	public static void main(String[] args) {
-		Locale.setDefault(Locale.US);
-		Scanner scn = new Scanner(System.in);
-		
-		DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-		
-		System.out.println("Entre com os dados do contrato:");
-		System.out.print("Número:");
-		int number = scn.nextInt();
-		System.out.print("Data (dd/MM/yyyy):");
-		LocalDate date = LocalDate.parse(scn.next(), fmt);
-		System.out.print("Valor do contrato:");
-		double totalValue = scn.nextDouble();
-		
-		Contract obj = new Contract(number, date, totalValue);
-		
-		System.out.print("Entre com o número de parcelas:");
-		int n = scn.nextInt();
-		
-		ContractService contractService = new ContractService(new PaypalService());
-		contractService.processContract(obj, n);
-		
-		System.out.println("Parcelas:");
-		for(Installment installment : obj.getInstalments()) {
-			System.out.println(installment);
-		}
-		
->>>>>>> 659df38809444a8a972985bbb48a5d5a6b016df6
+		sc.close();
 	}
 }
--- src/module-info.java
+++ src/module-info.java
@@ -1,6 +1,4 @@
-<<<<<<< HEAD
-module Interface {
-=======
-module Account {
->>>>>>> 659df38809444a8a972985bbb48a5d5a6b016df6
+
+module Interface{
+	
 }
